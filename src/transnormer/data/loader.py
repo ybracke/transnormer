5baa2dae8e0ed661bc6b02304e8806645884a0e5
Yannic Bracke <yannic.bracke@bbaw.de>
2023-02-21 14:31:25 +0100

    Loading data from XML version of DTAEval

--- src/transnormer/data/loader.py
+++ src/transnormer/data/loader.py
@@ -1,10 +1,13 @@
-import datasets
 from functools import wraps
 import itertools
 import glob
 import os
 import time
-from typing import Generator, TextIO, Union, List
+from typing import Generator, TextIO, Union, List, Tuple
+
+import datasets
+from lxml import etree
+from nltk.tokenize.treebank import TreebankWordDetokenizer
 
 
 # Helper function
@@ -20,45 +23,23 @@ def timer(func):
     return wrapper
 
 
-@timer
-def load_dtaeval_all() -> datasets.DatasetDict:
-    # TODO
-    datadir = "/home/bracke/data/dta/dtaeval/split-v3.1/txt"
+def file_gen(path: str) -> Generator[TextIO, None, None]:
+    """Yields file(s) from a path, where path can be file, dir or glob"""
 
-    train_path = os.path.join(datadir, "train")
-    validation_path = os.path.join(datadir, "dev")
-    test_path = os.path.join(datadir, "test")
-
-    ds = datasets.DatasetDict()
-    ds["train"] = load_dtaeval_as_dataset(train_path)
-    ds["validation"] = load_dtaeval_as_dataset(validation_path)
-    ds["test"] = load_dtaeval_as_dataset(test_path)
-
-    return ds
-
-
-def load_dtaeval_as_dataset(path: str) -> datasets.Dataset:
-    """
-    Load the file(s) under `path` into a datasets.Dataset with columns "orig"
-    and "norm"
-
-    If `path` is a directory name,
-    """
-
-    docs = [load_tsv(file, keep_sentences=True) for file in file_gen(path)]
-    docs_sent_joined = [
-        [[" ".join(sent) for sent in column] for column in doc] for doc in docs
-    ]
-
-    all_sents_orig, all_sents_norm = [], []
-    for doc_orig, doc_norm in docs_sent_joined:
-        all_sents_orig.extend([sent for sent in doc_orig])
-        all_sents_norm.extend([sent for sent in doc_norm])
-
-    return datasets.Dataset.from_dict({"orig": all_sents_orig, "norm": all_sents_norm})
+    if os.path.isfile(path):
+        with open(path, "r", encoding="utf-8") as file:
+            yield file
+    elif os.path.isdir(path):
+        for filename in os.listdir(path):
+            with open(os.path.join(path, filename), "r", encoding="utf-8") as file:
+                yield file
+    else:
+        for filename in glob.glob(path):
+            with open(filename, "r", encoding="utf-8") as file:
+                yield file
 
 
-def load_tsv(
+def load_tsv_to_lists(
     file_obj: TextIO, keep_sentences: bool = True
 ) -> Union[List[List[List[str]]], List[List[str]]]:
     """
@@ -129,17 +110,125 @@ def load_tsv(
     return columns
 
 
-def file_gen(path: str) -> Generator[TextIO, None, None]:
-    """Yields file(s) from a path, where path can be file, dir or glob"""
+def load_dtaevalxml_to_lists(
+    file: Union[str, TextIO], filter_bad: bool = False, filter_classes: List[str] = []
+) -> Tuple[List[List[str]], List[List[str]]]:
+    """
+    Reads from a DTAEval XML file and returns two lists containing sentences as
+    lists of tokens.
+    """
+    old = []
+    new = []
 
-    if os.path.isfile(path):
-        with open(path, "r", encoding="utf-8") as file:
-            yield file
-    elif os.path.isdir(path):
-        for filename in os.listdir(path):
-            with open(os.path.join(path, filename), "r", encoding="utf-8") as file:
-                yield file
-    else:
-        for filename in glob.glob(path):
-            with open(filename, "r", encoding="utf-8") as file:
-                yield file
+    tree = etree.parse(file)
+    for s in tree.iterfind("//s"):
+        sent_old = []
+        sent_new = []
+        # skip sentences that have the @sbad attribute
+        if "sbad" in s.attrib:
+            continue
+        # Iterate over <w> elements that are immediate childs of <s>
+        # This excludes nested <w> (which are used in case of a tokenization
+        # mismatch between old and new)
+        for w in s.xpath("./w"):
+            # Skip tokens if they meet some criterion
+            tokclass = w.attrib.get("class")
+            if tokclass in filter_classes:
+                continue
+            if "bad" in w.attrib and filter_bad:
+                continue
+            # Skip tokens that have no @old version (= rare errors)
+            try:
+                sent_old.append(w.attrib["old"])
+            except KeyError:
+                continue
+            # Store @old as normalization if there is none (e.g. "Mur¬" -> "Mur¬")
+            try:
+                sent_new.append(w.attrib["new"])
+            except KeyError:
+                sent_new.append(w.attrib["old"])
+        # append non-empty sentences
+        if len(sent_old) and len(sent_new):
+            old.append(sent_old)
+            new.append(sent_new)
+    return (old, new)
+
+
+def load_dtaevalxml_as_dataset(path: str, **kwargs) -> datasets.Dataset:
+    """
+    Load the file(s) under `path` into a datasets.Dataset with columns "orig"
+    and "norm"
+    """
+
+    # docs:
+    # List <-- documents
+    #     List <-- columns
+    #         List <-- sentences
+    #             List <-- tokens
+    docs = [load_dtaevalxml_to_lists(file, **kwargs) for file in file_gen(path)]
+
+    detokenizer = TreebankWordDetokenizer()
+
+    # Put tokens back together into sentence strings
+    docs_sent_joined = [
+        [[detokenizer.detokenize(sent) for sent in column] for column in doc]
+        for doc in docs
+    ]
+
+    all_sents_orig, all_sents_norm = [], []
+    for doc_orig, doc_norm in docs_sent_joined:
+        all_sents_orig.extend([sent for sent in doc_orig])
+        all_sents_norm.extend([sent for sent in doc_norm])
+
+    return datasets.Dataset.from_dict({"orig": all_sents_orig, "norm": all_sents_norm})
+
+
+@timer
+def load_dtaevalxml_all(datadir, **kwargs) -> datasets.DatasetDict:
+    train_path = os.path.join(datadir, "train")
+    validation_path = os.path.join(datadir, "dev")
+    test_path = os.path.join(datadir, "test")
+
+    ds = datasets.DatasetDict()
+    ds["train"] = load_dtaevalxml_as_dataset(train_path, **kwargs)
+    ds["validation"] = load_dtaevalxml_as_dataset(validation_path, **kwargs)
+    ds["test"] = load_dtaevalxml_as_dataset(test_path, **kwargs)
+
+    return ds
+
+
+# deprecated functions
+
+# @timer
+# def load_dtaeval_all() -> datasets.DatasetDict:
+#     datadir = "/home/bracke/data/dta/dtaeval/split-v3.1/txt"
+
+#     train_path = os.path.join(datadir, "train")
+#     validation_path = os.path.join(datadir, "dev")
+#     test_path = os.path.join(datadir, "test")
+
+#     ds = datasets.DatasetDict()
+#     ds["train"] = load_dtaeval_as_dataset(train_path)
+#     ds["validation"] = load_dtaeval_as_dataset(validation_path)
+#     ds["test"] = load_dtaeval_as_dataset(test_path)
+
+#     return ds
+
+
+# def load_dtaeval_as_dataset(path: str) -> datasets.Dataset:
+#     """
+#     Load the file(s) under `path` into a datasets.Dataset with columns "orig"
+#     and "norm"
+#     """
+
+#     docs = [load_tsv_to_lists(file, keep_sentences=True) for file in file_gen(path)]
+#     docs_sent_joined = [
+#         [[" ".join(sent) for sent in column] for column in doc] for doc in docs
+#     ]
+
+#     all_sents_orig, all_sents_norm = [], []
+#     for doc_orig, doc_norm in docs_sent_joined:
+#         all_sents_orig.extend([sent for sent in doc_orig])
+#         all_sents_norm.extend([sent for sent in doc_norm])
+
+#     return datasets.Dataset.from_dict({"orig": all_sents_orig, "norm": all_sents_norm})
--- tests/test_data_loading.py
+++ tests/test_data_loading.py
@@ -1,35 +1,84 @@
-import datasets
-from transnormer.data.loader import load_dtaeval_as_dataset, load_dtaeval_all
+# import datasets
+from transnormer.data.loader import load_dtaevalxml_to_lists, load_dtaevalxml_as_dataset
 
-def test_load_dtaeval_as_dataset_with_file():
-    path = "tests/testdata/txt/arnima_invalide_1818-mini.txt"
-    dataset = load_dtaeval_as_dataset(path)
-    # document contains two sentences on either layer
-    assert len(dataset["orig"]) == 2
-    assert len(dataset["norm"]) == 2
+# def test_load_dtaeval_as_dataset_with_file():
+#     path = "tests/testdata/txt/arnima_invalide_1818-mini.txt"
+#     dataset = load_dtaeval_as_dataset(path)
+#     # document contains two sentences on either layer
+#     assert len(dataset["orig"]) == 2
+#     assert len(dataset["norm"]) == 2
 
-def test_load_dtaeval_as_dataset_with_dir():
-    path = "tests/testdata/txt/"
-    dataset = load_dtaeval_as_dataset(path)
-    # Sum of sentences in all documents in the folder
-    assert len(dataset["orig"]) == 3
-    assert len(dataset["norm"]) == 3
+# def test_load_dtaeval_as_dataset_with_dir():
+#     path = "tests/testdata/txt/"
+#     dataset = load_dtaeval_as_dataset(path)
+#     # Sum of sentences in all documents in the folder
+#     assert len(dataset["orig"]) == 3
+#     assert len(dataset["norm"]) == 3
 
-def test_load_dtaeval_as_dataset_with_glob():
-    path = "tests/testdata/txt/*.txt"
-    dataset = load_dtaeval_as_dataset(path)
-    # Sum of sentences in all documents in the folder
-    assert len(dataset["orig"]) == 3
-    assert len(dataset["norm"]) == 3
+# def test_load_dtaeval_as_dataset_with_glob():
+#     path = "tests/testdata/txt/*.txt"
+#     dataset = load_dtaeval_as_dataset(path)
+#     # Sum of sentences in all documents in the folder
+#     assert len(dataset["orig"]) == 3
+#     assert len(dataset["norm"]) == 3
 
-def test_load_dtaeval_all():
-    dataset_dict = load_dtaeval_all()
-    assert isinstance(dataset_dict, datasets.DatasetDict)
-    # print(dataset_dict)
-    for part in dataset_dict:
-        dataset_dict[part]
-        print(part)
-    #     assert len(dataset_dict[part]["norm"]) == len(part["orig"])
-    #     print(len(part["norm"]))
-    
+# def test_load_dtaeval_all():
+#     dataset_dict = load_dtaeval_all()
+#     assert isinstance(dataset_dict, datasets.DatasetDict)
 
+
+def test_load_dtaevalxml_to_lists_no_additional_filter():
+    path = "tests/testdata/xml/arnima_invalide_1818-mini.xml"
+    old, new = load_dtaevalxml_to_lists(path)
+    # document contains 4 sentences on either layer (orig, norm)
+    assert len(old) == 4
+    assert len(new) == 4
+    assert len(old[3]) == 15
+    assert len(new[3]) == 15
+
+
+def test_load_dtaevalxml_to_lists_filter_bad():
+    path = "tests/testdata/xml/arnima_invalide_1818-mini.xml"
+    old, new = load_dtaevalxml_to_lists(path, filter_bad=True)
+    assert len(old) == 4
+    assert len(new) == 4
+    assert len(old[3]) == 14
+    assert len(new[3]) == 14
+
+
+def test_load_dtaevalxml_to_lists_filter_classes_bug():
+    path = "tests/testdata/xml/arnima_invalide_1818-mini.xml"
+    old, new = load_dtaevalxml_to_lists(path, filter_bad=False, filter_classes=["BUG"])
+    assert len(old) == 4
+    assert len(new) == 4
+    assert len(old[3]) == 14
+    assert len(new[3]) == 14
+
+
+def test_load_dtaevalxml_as_dataset_with_file():
+    path = "tests/testdata/xml/arnima_invalide_1818-mini.xml"
+    dataset = load_dtaevalxml_as_dataset(path)
+    # document contains 4 sentences on either layer (orig, norm)
+    assert len(dataset["orig"]) == 4
+    assert len(dataset["norm"]) == 4
+
+
+def test_load_dtaevalxml_as_dataset_with_file_and_no_addtional_filter():
+    path = "tests/testdata/xml/arnima_invalide_1818-mini.xml"
+    dataset = load_dtaevalxml_as_dataset(path)
+    target = "rief Basset, er sprengt sich in die Luft, rettet Euch und Euer Kind!"
+    assert dataset["norm"][3] == target
+
+
+def test_load_dtaevalxml_as_dataset_with_file_and_filter_classes_bug():
+    path = "tests/testdata/xml/arnima_invalide_1818-mini.xml"
+    dataset = load_dtaevalxml_as_dataset(path, filter_classes=["BUG"])
+    target = "rief Basset, er sprengt sich in, rettet Euch und Euer Kind!"
+    assert dataset["norm"][3] == target
+
+
+# def test_load_dtaevalxml_all():
+#     kwargs = {"filter_classes":["BUG", "FM", "GRAPH"]}
+#     dataset_dict = load_dtaevalxml_all(**kwargs)
+#     print(dataset_dict)
+#     assert isinstance(dataset_dict, datasets.DatasetDict)
