face65e752c68e7b7921b4663a1d04ea71826831
Yannic Bracke <yannic.bracke@bbaw.de>
2023-02-27 16:21:59 +0100

    Reformat test_train_model

--- tests/test_train_model.py
+++ tests/test_train_model.py
@@ -1,16 +1,15 @@
-import datasets
 from transformers import AutoTokenizer
 
 from transnormer.models.train_model import tokenize_input_and_output
-from transnormer.data.loader import *
+from transnormer.data import loader
 
 import tomli
 
+
 def test_tokenize_input_and_output():
-    
     # Load data
     path = "./tests/testdata/dtaeval/txt/arnima_invalide_1818-head10.txt"
-    dataset = load_dtaeval_as_dataset(path)
+    dataset = loader.load_dtaeval_as_dataset(path)
 
     # Load tokenizers
     tokenizer_input = AutoTokenizer.from_pretrained(
@@ -19,11 +18,12 @@ def test_tokenize_input_and_output():
     tokenizer_labels = AutoTokenizer.from_pretrained("bert-base-multilingual-cased")
 
     # Parameters for tokenizing input and labels
-    fn_kwargs = {"tokenizer_input": tokenizer_input, 
-                 "tokenizer_output": tokenizer_labels,
-                 "max_length_input": 128,
-                 "max_length_output": 128,
-                 }
+    fn_kwargs = {
+        "tokenizer_input": tokenizer_input,
+        "tokenizer_output": tokenizer_labels,
+        "max_length_input": 128,
+        "max_length_output": 128,
+    }
 
     # Do tokenization via mapping
     prepared_dataset = dataset.map(
@@ -32,48 +32,45 @@ def test_tokenize_input_and_output():
         batched=True,
         batch_size=1,
         remove_columns=["orig", "norm"],
-    )   
-    
-    assert(len(prepared_dataset[0]["input_ids"])==128)
-    assert(len(prepared_dataset[0]["attention_mask"])==128)
-    assert(len(prepared_dataset[0]["labels"])==128)
+    )
+
+    assert len(prepared_dataset[0]["input_ids"]) == 128
+    assert len(prepared_dataset[0]["attention_mask"]) == 128
+    assert len(prepared_dataset[0]["labels"]) == 128
+
 
 def test_parameter_loading():
     target_param_dict = {
-        "gpu" : "cuda:0",
-        "random_seed" : 42,
-        "data" : {
-            "path" : "./data/dta/dtaeval/split-v3.1/txt",
+        "gpu": "cuda:0",
+        "random_seed": 42,
+        "data": {
+            "path": "./data/dta/dtaeval/split-v3.1/txt",
         },
-        "subset_sizes" : {
-            "train" : 100,
-            "validation" : 10,
-            "test" : 1
+        "subset_sizes": {"train": 100, "validation": 10, "test": 1},
+        "tokenizer": {
+            "max_length_input": 128,
+            "max_length_output": 128,
         },
-        "tokenizer" : {
-            "max_length_input" : 128,
-            "max_length_output" : 128,
+        "language_models": {
+            "checkpoint_encoder": "dbmdz/bert-base-historic-multilingual-cased",
+            "checkpoint_decoder": "bert-base-multilingual-cased",
         },
-        "language_models" : {
-            "checkpoint_encoder" : "dbmdz/bert-base-historic-multilingual-cased",
-            "checkpoint_decoder" : "bert-base-multilingual-cased"
+        "training_hyperparams": {
+            "batch_size": 10,
+            "epochs": 10,
+            "eval_steps": 1000,
+            "eval_strategy": "steps",
+            "save_steps": 10,
+            "fp16": True,
         },
-        "training_hyperparams" : {
-            "batch_size" : 10,
-            "epochs" : 10,
-            "eval_steps" : 1000,
-            "eval_strategy" : "steps",
-            "save_steps" : 10,
-            "fp16" : True,
+        "beam_search_decoding": {
+            "no_repeat_ngram_size": 3,
+            "early_stopping": True,
+            "length_penalty": 2.0,
+            "num_beams": 4,
         },
-        "beam_search_decoding" : {
-            "no_repeat_ngram_size" : 3,
-            "early_stopping" : True,
-            "length_penalty" : 2.0,
-            "num_beams" : 4,
-            }
     }
-    
+
     path = "training_config_template.toml"
     with open(path, mode="rb") as fp:
         CONFIGS = tomli.load(fp)
