b1001b9d9b48859ed7681ce7f3b0cc3187e8fe29
Yannic Bracke <yannic.bracke@bbaw.de>
2023-10-06 09:21:43 +0200

    Remove unneeded functions from generate script

--- src/transnormer/models/generate.py
+++ src/transnormer/models/generate.py
@@ -12,109 +12,6 @@ import transformers
 from transnormer.preprocess import translit
 
 
-def tokenize_input_and_output(
-    batch, tokenizer_input, tokenizer_output, max_length_input, max_length_output
-):
-    """
-    Tokenizes a `batch` of input and label strings. Assumes that input string
-    (label string) is stored in batch under the key `"orig"` (`"norm"`).
-
-    Function is inspired by `process_data_to_model_inputs` described here:
-    https://huggingface.co/blog/warm-starting-encoder-decoder#warm-starting-the-encoder-decoder-model
-    """
-
-    # Tokenize the inputs and labels
-    inputs = tokenizer_input(
-        batch["orig"],
-        padding="max_length",
-        truncation=True,
-        max_length=max_length_input,
-    )
-    outputs = tokenizer_output(
-        batch["norm"],
-        padding="max_length",
-        truncation=True,
-        max_length=max_length_output,
-    )
-
-    batch["input_ids"] = inputs.input_ids
-    batch["attention_mask"] = inputs.attention_mask
-    batch["labels"] = outputs.input_ids.copy()
-
-    # Make sure that the PAD token is ignored
-    batch["labels"] = [
-        [-100 if token == tokenizer_output.pad_token_id else token for token in labels]
-        for labels in batch["labels"]
-    ]
-
-    return batch
-
-
-# TODO: For simplicity I just copied and adjusted this function from train_model.py
-#       Adjust it so that it can be used in both training and prediction
-def tokenize_datasets(
-    dataset: datasets.DatasetDict, configs: Dict[str, Any]
-) -> Tuple[
-    datasets.DatasetDict,
-    transformers.PreTrainedTokenizerBase,
-    transformers.PreTrainedTokenizerBase,
-]:
-    """
-    Tokenize the datasets in a DatasetDict as specified in the config file.
-
-    Also returns the loaded input and output tokenizers.
-    """
-
-    # (1) Load tokenizers
-    #  (1.1) Load input and output tokenizer
-    tokenizer_input = transformers.AutoTokenizer.from_pretrained(
-        configs["tokenizer"]["checkpoint_in"]
-    )
-    if "checkpoint_out" in configs["tokenizer"]:
-        tokenizer_output = transformers.AutoTokenizer.from_pretrained(
-            configs["tokenizer"]["checkpoint_out"]
-        )
-    else:
-        # Output tokenizer is simply a reference to input tok
-        tokenizer_output = tokenizer_input
-
-    # (1.2) Optional: replace tokenizer's normalization component with a custom transliterator
-    if "input_transliterator" in configs["tokenizer"]:
-        if configs["tokenizer"]["input_transliterator"] == "Transliterator1":
-            transliterator = translit.Transliterator1()
-        else:
-            transliterator = None
-        tokenizer_input = translit.exchange_transliterator(
-            tokenizer_input, transliterator
-        )
-
-    # (3) Define tokenization keyword arguments
-    tokenization_kwargs = {
-        "tokenizer_input": tokenizer_input,
-        "tokenizer_output": tokenizer_output,
-        "max_length_input": configs["tokenizer"]["max_length_input"],
-        "max_length_output": configs["tokenizer"]["max_length_output"],
-    }
-
-    # Tokenize by applying map function to the DatasetDict
-    prepared_dataset = dataset.map(
-        tokenize_input_and_output,
-        fn_kwargs=tokenization_kwargs,
-        remove_columns=["orig", "norm"],
-        batched=True,
-        batch_size=configs["generation"]["batch_size"],
-        load_from_cache_file=False,
-    )
-
-    # Convert to torch tensors
-    prepared_dataset.set_format(
-        type="torch",
-        columns=["input_ids", "attention_mask", "labels"],
-    )
-
-    return prepared_dataset, tokenizer_input, tokenizer_output
-
-
 def parse_and_check_arguments(
     arguments: Optional[List[str]] = None,
 ) -> argparse.Namespace:
@@ -170,8 +67,27 @@ def main(arguments: Optional[List[str]] = None) -> None:
     # ds["train"] = ds["train"].rename_column("text", "orig")
     # ds["train"] = ds["train"].rename_column("norm_manual", "norm")
 
-    # Tokenize data
-    prepared_dataset, tokenizer_input, tokenizer_output = tokenize_datasets(ds, CONFIGS)
+    # Load tokenizer(s)
+    tokenizer_input = transformers.AutoTokenizer.from_pretrained(
+        CONFIGS["tokenizer"]["checkpoint_in"]
+    )
+    if "checkpoint_out" in CONFIGS["tokenizer"]:
+        tokenizer_output = transformers.AutoTokenizer.from_pretrained(
+            CONFIGS["tokenizer"]["checkpoint_out"]
+        )
+    else:
+        # Output tokenizer is simply a reference to input tok
+        tokenizer_output = tokenizer_input
+
+    # Optional: replace tokenizer's normalization component with a custom transliterator
+    if "input_transliterator" in CONFIGS["tokenizer"]:
+        if CONFIGS["tokenizer"]["input_transliterator"] == "Transliterator1":
+            transliterator = translit.Transliterator1()
+        else:
+            transliterator = None
+        tokenizer_input = translit.exchange_transliterator(
+            tokenizer_input, transliterator
+        )
 
     # Load model
     checkpoint = CONFIGS["model"]["checkpoint"]
