3db4d1804a75bc1c1cf10a12aa8e07f07261313e
Yannic Bracke <yannic.bracke@bbaw.de>
2024-04-25 16:17:21 +0200

    Update metrics computation

--- src/transnormer/evaluation/metrics.py
+++ src/transnormer/evaluation/metrics.py
@@ -101,6 +101,17 @@ def word_acc_selected_target_types(
 ) -> float:
     """Accuracy for selected target (i.e. normalized) types over the entire corpus, e.g.
     known or unknown types"""
+    correct, total = n_correct_and_total_selected_target_types(
+        alignments, selected_types, deselected_types
+    )
+    return correct / total
+
+
+def n_correct_and_total_selected_target_types(
+    alignments: List[List[Tuple[str, str, float]]],
+    selected_types: Optional[Set[str]] = None,
+    deselected_types: Optional[Set[str]] = None,
+) -> Tuple[int, int]:
     if deselected_types is None:
         deselected_types = set()
     correct_corpus, total_corpus = 0, 0
@@ -115,10 +126,10 @@ def word_acc_selected_target_types(
             if selected_types is None or word[0] in selected_types:
                 if word[0] == word[1]:
                     correct_sent += 1
-            total_sent += 1
+                total_sent += 1
         correct_corpus += correct_sent
         total_corpus += total_sent
-    return correct_corpus / total_corpus
+    return correct_corpus, total_corpus
 
 
 def word_acc_selected_source_types(
@@ -131,6 +142,18 @@ def word_acc_selected_source_types(
     Accuracy for selected source (i.e. historical) types over the entire corpus, e.g.
     known types, unknown types or ambiguous types (i.e. historical type with multiple possible normalizations).
     """
+    correct, total = n_correct_and_total_selected_source_types(
+        alignments_orig2gold, alignments_orig2pred, selected_types, deselected_types
+    )
+    return correct / total
+
+
+def n_correct_and_total_selected_source_types(
+    alignments_orig2gold: List[List[Tuple[str, str, float]]],
+    alignments_orig2pred: List[List[Tuple[str, str, float]]],
+    selected_types: Optional[Set[str]] = None,
+    deselected_types: Optional[Set[str]] = None,
+) -> Tuple[int, int]:
     if deselected_types is None:
         deselected_types = set()
     correct_corpus, total_corpus = 0, 0
@@ -149,10 +172,11 @@ def word_acc_selected_source_types(
                 if orig1 in selected_types:
                     if gold == pred:
                         correct_sent += 1
+                    total_sent += 1
             else:
                 if gold == pred:
                     correct_sent += 1
-            total_sent += 1
+                total_sent += 1
         correct_corpus += correct_sent
         total_corpus += total_sent
-    return correct_corpus / total_corpus
+    return correct_corpus, total_corpus
