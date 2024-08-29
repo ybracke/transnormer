b3d2028fa22da3ea97ef042bf588a37056fdeb04
Yannic Bracke <yannic.bracke@bbaw.de>
2024-08-29 15:33:40 +0200

    Version of train_model.py without byt5-irrelevant parts

---
+++ train_model.sh
@@ -0,0 +1,11 @@
+# Exit in case of errors
+set -e
+
+# Prepare conda environment
+CONDA_BASE=$(conda info --base)
+source $CONDA_BASE/etc/profile.d/conda.sh
+conda activate gpu-venv-transnormer
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CONDA_PREFIX/lib/
+export CUDA_VISIBLE_DEVICES=1
+
+python3 src/transnormer/models/train_byt5model.py
